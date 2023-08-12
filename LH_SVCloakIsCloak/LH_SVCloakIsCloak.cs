138282ded6d80265f1097bd62f748ad9ce728244
xlady <xlady@WINDOWS-UQIOVV9>
2023-08-12 18:29:09 +0100

    Add project files.

---
+++ LH_SVCloakIsCloak/LH_SVCloakIsCloak.cs
@@ -0,0 +1,34 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LH_SVCloakIsCloak
+{
+    [BepInPlugin(pluginGuid, pluginName, pluginVersion)]
+
+    public class LH_SVCloakIsCloak : BaseUnityPlugin
+    {
+        public const string pluginGuid = "LH_SVCloakIsCloak";
+        public const string pluginName = "LH_SVCloakIsCloak";
+        public const string pluginVersion = "0.0.1";
+
+        public void Awake()
+        {
+            Harmony.CreateAndPatchAll(typeof(LH_SVCloakIsCloak));
+        }
+
+        [HarmonyPatch(typeof(SpaceShip), nameof(SpaceShip.RemoveCloak))]
+        [HarmonyPrefix]
+        static bool PreventDecloak(PlayerControl ___pc, List<MeleeWeaponControl> ___meleeControls)
+        {
+            if (!___pc)
+                return true;
+            else if (___meleeControls.Count > 0)
+                return true;
+            else
+                return false;
+        }
+    }
+}
+
